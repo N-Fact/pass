df9dd0ecc043bfa36592fa87a3821891390f6e5f
tekin7 <tekin_7_@hotmail.com>
2022-04-22 22:55:12 +0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4783095a9ee29df3"; }
+/******/ 		__webpack_require__.h = function() { return "d4d8cf501d95959e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
